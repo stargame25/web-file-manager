835602305f77f321951e028a47ac4fe48895f4de
stargame25 <andriy2033@gmail.com>
2019-10-23 02:34:36 +0400

    Version 0.6
    Updated CSS styles
    Improved mobile support
    Improved responsive and adaptive design

--- filemanager/static/js/transfer.js
+++ filemanager/static/js/transfer.js
@@ -275,6 +275,14 @@ function onProgressTrigger(event) {
     span.textContent = percent + "%" + " (" + size_loaded + " MB of " + size_full + " MB)";
 }
 
+function bytesToSize(bytes) {
+   let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   if (bytes === 0) return '0 Byte';
+   let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
+
+
 function onErrorTrigger() {
     toggleLoading();
     filesInput.value = "";
